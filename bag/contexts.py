973e09454ef756730cd6a5eb863c9a7b03abefc8
AlrxDaley <alexander_daley@icloud.com>
2022-04-11 18:45:02 +0100

    added bag quantity and size features

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,22 +1,47 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import product
 
 from boutique_ado.settings import FREE_DELIVERY_THRESHOLD
 
 
 def bag_contents(request):
-    
+
     bag_items = []
     total = 0
     product_count = 0
-    
+    bag = request.session.get('bag', {})
+
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
+
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-        
+    
     grand_total = delivery + total
     
     context = {
@@ -28,5 +53,5 @@ def bag_contents(request):
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
     }
-    
+
     return context
